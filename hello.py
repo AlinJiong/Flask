9fa46ea6cfcdc57c54591ba3bfdd3ca8a44e9583
AlinJiong <alinjiong@qq.com>
2021-12-25 22:50:21 +0800

    模板

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
@@ -30,6 +30,23 @@ def hello_user(name):
         return redirect(url_for('hello_admin'))
     else:
         return redirect(url_for('hello_guest', guest=name))
+
+
+@app.route('/index')
+def index():
+    # 往模板中传入的数据
+    my_str = 'Hello Word'
+    my_int = 10
+    my_array = [3, 4, 2, 1, 7, 9]
+    my_dict = {'name': 'xiaoming', 'age': 18}
+    # return render_template('index.html',
+    #                        my_str=my_str,
+    #                        my_int=my_int,
+    #                        my_array=my_array,
+    #                        my_dict=my_dict)
+
+    #拆包传入数据
+    return render_template('index.html', **locals())
 
 
 # URL, 反向路由url_for, func
