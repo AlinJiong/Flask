c7118a3b62271a77d9826da15a296b230c515023
AlinJiong <alinjiong@qq.com>
2021-12-26 22:09:35 +0800

    session应用，要设置app.secret_key

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask.helpers import make_response
+from flask import session
 
 app = Flask(__name__)
+app.secret_key = 'fkdjsafjdkfdlkjfadskjfadskljdsfklj'
 
 
 # 127.0.0.1:5000/
@@ -86,6 +88,38 @@ def del_cookie():
     resp = make_response('del cookies')
     resp.delete_cookie('alin')
     return resp
+
+
+# session
+@app.route('/home')
+def home():
+    if 'username' in session:
+        username = session['username']
+        return '登录用户名是:' + username + '<br>' + \
+            "<b><a href = '/logout'>点击这里注销</a></b>"
+
+    return "您暂未登录， <br><a href = '/login'></b>" + \
+         "点击这里登录</b></a>"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+
+    return '''
+                <form action = "" method = "post">
+                    <p><input type="text" name="username"/></p>
+                    <p><input type="submit" value ="登录"/></p>
+                </form>
+            '''
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 
 # URL, 反向路由url_for, func
